b31f5ba64b3bf31e9a08f422d3a2e1ab1bf7baf4
Anka <runner@sjc22-bm211-fd46dde4-c9ce-4363-b29e-94ace5b2aae2-1EDD71351877.local>
2026-04-17 23:27:39 +0000

    Update focally cask to v0.2.1

--- Casks/focally.rb
+++ Casks/focally.rb
@@ -1,6 +1,6 @@
 cask "focally" do
   version "0.2.1"
-  sha256 "6feec5154bb54bb48eeb34191c3887f09cd63de0b0370137a1fbbbe59b09da1d"
+  sha256 "b674bdc09c72264302dd0996c2f2c493f633307917402991f28bf1035b580419"
 
   url "https://github.com/EliabLemus/focally/releases/download/v#{version}/Focally-#{version}.dmg"
   name "Focally"
