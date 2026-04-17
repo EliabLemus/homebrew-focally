0ed3544d64b0d73cf76a4496cd69480409eba2fd
Eliab Lemus <eliab@openjaime.dev>
2026-04-17 15:45:41 -0600

    Update sha256 v0.2.0 signed

--- Casks/focally.rb
+++ Casks/focally.rb
@@ -1,6 +1,6 @@
 cask "focally" do
   version "0.2.0"
-  sha256 "a929ffffe8ae34dfa51bae79a6d456c741f1e63739d575ca41fec141335ce752"
+  sha256 "628cdb4557967a036e23ef5806cae5da77bde1bd7d10bf018d99814a8860f95f"
 
   url "https://github.com/EliabLemus/focally/releases/download/v#{version}/Focally-#{version}.dmg"
   name "Focally"
