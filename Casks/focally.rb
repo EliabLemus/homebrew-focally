f9bf975eaea968c7a24b4973bf2d15afee09c2ce
Eliab Lemus <eliab@openjaime.dev>
2026-04-17 15:44:24 -0600

    Update sha256 for v0.2.0 with icon

--- Casks/focally.rb
+++ Casks/focally.rb
@@ -1,6 +1,6 @@
 cask "focally" do
   version "0.2.0"
-  sha256 "8a9b9e5f9effd376950acc51c5de49eefdf79d06c120f242df0886d4940b5235"
+  sha256 "a929ffffe8ae34dfa51bae79a6d456c741f1e63739d575ca41fec141335ce752"
 
   url "https://github.com/EliabLemus/focally/releases/download/v#{version}/Focally-#{version}.dmg"
   name "Focally"
